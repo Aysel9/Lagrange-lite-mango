5a1449423f0bee2b8dbe0aedc2bbd2cacd7813a0
Eray Bartan <ebartan@gmail.com>
2022-03-07 20:13:04 +0300

    assets.coingecko.com

--- next.config.js
+++ next.config.js
@@ -1,51 +1,14 @@
 
 /** @type {import('next').NextConfig} */
 
-const withPlugins = require("next-compose-plugins");
 const { i18n } = require('./next-i18next.config')
 
 
-/** eslint-disable @typescript-eslint/no-var-requires */
-const withTM = require("next-transpile-modules")([
-  "@solana/wallet-adapter-base",
-  // Uncomment wallets you want to use
-  // "@solana/wallet-adapter-bitpie",
-  // "@solana/wallet-adapter-coin98",
-  // "@solana/wallet-adapter-ledger",
-  // "@solana/wallet-adapter-mathwallet",
-  "@solana/wallet-adapter-phantom",
-  "@solana/wallet-adapter-react",
-  "@solana/wallet-adapter-solflare",
-  "@solana/wallet-adapter-sollet",
-  // "@solana/wallet-adapter-solong",
-  // "@solana/wallet-adapter-torus",
-  "@solana/wallet-adapter-wallets",
-  // "@project-serum/sol-wallet-adapter",
-  // "@solana/wallet-adapter-ant-design",
-]);
-
-
-
-const plugins = [
-  // add this if you need LESS
-  // [withLess, {
-  //   lessLoaderOptions: {
-  //     /* ... */
-  //   },
-  // }],
-  [
-    withTM,
-    {
-      webpack5: true,
-      reactStrictMode: true,
-      images: {
-        domains: ['firebasestorage.googleapis.com', 'avatars.dicebear.com', 'raw.githubusercontent.com', 's2.coinmarketcap.com', 'images.unsplash.com', 'avatars.githubusercontent.com', 'relaxed-newton-b77a1a.netlify.app'],
-      },
-    },
-  ],
-];
 
 const moduleExports = {
+  images: {
+    domains: ['s2.coinmarketcap.com','assets.coingecko.com'],
+  },
   i18n,
   async redirects() {
     return [
